15f57a2b49688e1198f40d5451e7ce9eef0bfdec
Dominickk <dominickeehan@gmail.com>
2021-05-10 20:20:15 +1200

    Desnity plot draft

---
+++ light-curve-test.py
@@ -0,0 +1,58 @@
+import MulensModel as mm
+import matplotlib.pyplot as plt
+import numpy as np
+
+def chi2_for_model(theta, event, parameters_to_fit):
+    """for given event set attributes from parameters_to_fit
+    (list of str) to values from the theta list"""
+    for (key, parameter) in enumerate(parameters_to_fit):
+        setattr(event.model.parameters, parameter, theta[key])
+    return event.get_chi2()
+
+my_1S2L_model = mm.Model({'t_0': 2452848.06, 'u_0': 0.133,
+     't_E': 61.5, 'rho': 0.00096, 'q': 0.0039, 's': 1.120,
+     'alpha': 223.8})
+
+my_1S2L_model.set_magnification_methods([2452833., 'VBBL', 2452845.])
+t=my_1S2L_model.set_times()
+my_data = mm.MulensData(data_list=[t, my_1S2L_model.magnification(t), my_1S2L_model.magnification(t)*0+0.03])
+
+
+
+def func(t_0, u_0, t_E, rho, q, s, alpha, my_data):
+    my_f_model = mm.Model({'t_0': t_0, 'u_0': u_0,
+     't_E': t_E, 'rho': rho, 'q': q, 's': s,
+     'alpha': alpha})
+    my_f_model.set_magnification_methods([2452833., 'VBBL', 2452845.])
+
+    my_event = mm.Event(datasets=my_data, model=my_f_model)
+    return my_event.get_chi2()
+
+qaxis = np.linspace(0.001, 0.5, 20)
+saxis = np.linspace(1, 2, 20)
+result=np.zeros((20,20))
+x=-1
+y=-1
+for i in qaxis:
+    x=x+1
+    y=-1
+    for j in saxis:
+        y=y+1
+        result[x][y] = func(2452848.06, 0.133, 61.5, 0.00096, i, j, 223.8, my_data)
+
+plt.imshow(result, cmap='hot', interpolation='nearest')
+plt.savefig('books_read.png')
+
+
+
+#parameters_to_fit = ['t_0', 'u_0', 't_E', 'rho', 'q', 's', 'alpha']
+#initial_guess = [t_0, u_0, t_E, rho, q, s, alpha]
+#initial_guess = [2452848.06, 0.133, 61.5, 0.00096, 0.0039, 1.120, 223.8]
+
+
+#print(chi2_for_model(initial_guess, my_event, parameters_to_fit))
+
+#plt.figure()
+#my_1S2L_model.plot_magnification(t_range=[2452810, 2452890],
+#    subtract_2450000=True, color='black')
+#plt.savefig('books_read.png')
