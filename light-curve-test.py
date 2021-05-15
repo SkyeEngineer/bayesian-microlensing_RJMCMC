732fc339327c2019229d6d61c2f03f823c0e0e4c
Dominickk <dominickeehan@gmail.com>
2021-05-15 13:41:05 +1200

    Parameter heatmap

--- light-curve-test.py
+++ light-curve-test.py
@@ -15,9 +15,9 @@ my_1S2L_model = mm.Model({'t_0': 2452848.06, 'u_0': 0.133,
 
 my_1S2L_model.set_magnification_methods([2452833., 'VBBL', 2452845.])
 t=my_1S2L_model.set_times()
-my_data = mm.MulensData(data_list=[t, my_1S2L_model.magnification(t), my_1S2L_model.magnification(t)*0+0.03])
+my_data = mm.MulensData(data_list=[t, my_1S2L_model.magnification(t), my_1S2L_model.magnification(t)*0+0.003]) #orignally 0.03
 
-
+print(my_1S2L_model.magnification(t))
 
 def func(t_0, u_0, t_E, rho, q, s, alpha, my_data):
     my_f_model = mm.Model({'t_0': t_0, 'u_0': u_0,
@@ -28,9 +28,28 @@ def func(t_0, u_0, t_E, rho, q, s, alpha, my_data):
     my_event = mm.Event(datasets=my_data, model=my_f_model)
     return my_event.get_chi2()
 
-qaxis = np.linspace(0.001, 0.5, 20)
-saxis = np.linspace(1, 2, 20)
-result=np.zeros((20,20))
+de=15
+toaxis = np.linspace(2452843.06, 2452853.06, de)
+teaxis = np.linspace(56.5, 66.5, de)
+result=np.zeros((de,de))
+x=-1
+y=-1
+for i in toaxis:
+    x=x+1
+    y=-1
+    for j in teaxis:
+        y=y+1
+        result[x][y] = func(i, 0.133, j, 0.00096, 0.0039, 1.120, 223.8, my_data)
+
+plt.imshow(result, cmap='hot', interpolation='none', extent=[56.5, 66.5, 2853.06, 2843.06,])
+plt.xlabel('te [days]') # Set the y axis label of the current axis.
+plt.ylabel('to-2450000 [days]') # Set a title.
+plt.title('Closest Approach/Crossing Time Chi Squared over parameter space')
+plt.savefig('teto.png')
+
+qaxis = np.linspace(0.001, 0.2, de)
+saxis = np.linspace(1, 1.3, de)
+result=np.zeros((de,de))
 x=-1
 y=-1
 for i in qaxis:
@@ -40,9 +59,11 @@ for i in qaxis:
         y=y+1
         result[x][y] = func(2452848.06, 0.133, 61.5, 0.00096, i, j, 223.8, my_data)
 
-plt.imshow(result, cmap='hot', interpolation='nearest')
-plt.savefig('books_read.png')
-
+plt.imshow(result, cmap='hot', interpolation='none', extent=[1, 1.3, 0.2, 0.001,])
+plt.xlabel('s [Einstein Ring Radius]') # Set the y axis label of the current axis.
+plt.ylabel('q') # Set a title.
+plt.title('Separation/Mass Fraction Chi Squared over parameter space')
+plt.savefig('sq.png')
 
 
 #parameters_to_fit = ['t_0', 'u_0', 't_E', 'rho', 'q', 's', 'alpha']
