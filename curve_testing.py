fixed standard deviation

curve_testing.py
@@ -13,7 +13,7 @@ import plotting as pltf
 import random
 import numpy as np
 import matplotlib.pyplot as plt
-import get_neural_network as neural_net
+#import get_neural_network as neural_net
 from copy import deepcopy
 
 import time
@@ -25,15 +25,15 @@ import time
 #-----------
 
 
-suite_n = 1
+suite_n = 0
 adapt_MH_warm_up = 25 #25 # mcmc steps without adaption
-adapt_MH = 75  #475 # mcmc steps with adaption
+adapt_MH = 975  #475 # mcmc steps with adaption
 initial_n = 1 # times to repeat mcmc optimisation of centers to try to get better estimate
-iterations = 100 # rjmcmc steps
+iterations = 10000 # rjmcmc steps
 n_epochs = 720
 epochs = np.linspace(0, 72, n_epochs + 1)[:n_epochs]
 sn_base = 23 #(230-23)/2 + 23 # np.random.uniform(23.0, 230.0) # lower means noisier
-n_pixels = 3 # density for posterior contour plot
+n_pixels = 20 # density for posterior contour plot
 n_sampled_curves = 5 # sampled curves for viewing distribution of curves
 uniform_priors = False 
 informative_priors = True
@@ -94,7 +94,7 @@ else: # use known values for centers
 
     binary_centers = [
     [1.50424747e+01, 1.04854599e-01, 1.00131283e+01, 4.51699379e-05, 9.29979384e-01, 8.72737579e+01], # 0
-    [15.0245, 0.1035, 10**1.0063, 10**-2.3083, 10**0.5614, 55.2111],    # 1
+    [15.0245, 0.1035, 10**1.0063, 10**-2.3083, 10**0.5614, 161.0036],    # 1
     [15.0186, 0.1015, 10**1.0050, 10**-1.9734, 10**-0.3049, 60.4598],  # 2
     [14.9966, 0.1020, 10**1.0043, 10**-1.9825, 10**-0.1496, 60.2111]]   # 3
     binary_center = sampling.State(truth = np.array(binary_centers[suite_n]))
@@ -114,6 +114,7 @@ binary_Model = sampling.Model(1, 6, binary_center, priors, binary_covariance, da
 Models = [single_Model, binary_Model]
 
 start_time = (time.time())
+random.seed(42)
 joint_model_chain, total_acc, inter_info = sampling.adapt_RJMH(Models, adapt_MH_warm_up, adapt_MH, initial_n, iterations, user_feedback)
 duration = (time.time() - start_time)/60
 print(duration, ' minutes')
@@ -138,7 +139,7 @@ pltf.adaption_contraction(inter_info, iterations, name+'-inter', dpi)
 
 
 
-#acf.plot_act(joint_model_chain, symbols, name, dpi)
+acf.plot_act(joint_model_chain, symbols, name, dpi)
 #acf.attempt_truncation(Models, joint_model_chain)
 sampling.output_file(Models, joint_model_chain, total_acc, n_epochs, sn_base, letters, name, event_params)
 
@@ -150,6 +151,6 @@ plt.locator_params(axis = "y", nbins = 2) # only two ticks
 plt.savefig('results/'+name+'-mtrace.png', bbox_inches = 'tight', dpi = dpi)
 plt.clf()
 
-pltf.density_heatmaps(binary_Model, 3, data, event_params, symbols, 1, name, dpi)
-#pltf.joint_samples_pointilism(binary_Model, single_Model, joint_model_chain, symbols, name, dpi)
+pltf.density_heatmaps(binary_Model, n_pixels, data, event_params, symbols, 1, name, dpi)
+pltf.joint_samples_pointilism(binary_Model, single_Model, joint_model_chain, symbols, name, dpi)
 #pltf.center_offsets_pointilism(binary_Model, single_Model, shifted_symbols, name, dpi)

robustness.py
@@ -10,7 +10,7 @@ import plotting as pltf
 import random
 import numpy as np
 import matplotlib.pyplot as plt
-import get_neural_network as neural_net
+#import get_neural_network as neural_net
 from copy import deepcopy
 
 import time
@@ -25,9 +25,9 @@ def P_m2(event_params, sn_base, n_epochs):
     #-----------
 
     adapt_MH_warm_up = 25 #25 # mcmc steps without adaption
-    adapt_MH = 75 #475 # mcmc steps with adaption
+    adapt_MH = 475 #475 # mcmc steps with adaption
     initial_n = 1 #5 # times to repeat mcmc optimisation of centers to try to get better estimate
-    iterations = 100 # rjmcmc steps
+    iterations = 5000 # rjmcmc steps
 
     truncate = False # automatically truncate burn in period based on autocorrelation of m
 
@@ -65,7 +65,7 @@ def P_m2(event_params, sn_base, n_epochs):
 
     else: # use known values for centers 
 
-        binary_center = sampling.State(truth = np.concatenate((np.array(event_params.truth),[0.001, 0.2, 0])))
+        binary_center = sampling.State(truth = np.concatenate((np.array(event_params.truth),[0.001, 2.5, 0])))
         single_center = sampling.State(truth = np.array(event_params.truth))
 
     #single_center = sampling.State(truth = neural_net.get_model_centers(neural_net.get_posteriors(0), data.flux))
@@ -94,16 +94,16 @@ def P_m2(event_params, sn_base, n_epochs):
 
 
 theta = [36, 1.0, 5.5]
-n = 2
+n = 5
 tE_pi = distributions.Truncated_Log_Normal(1, 100, 10**1.15, 10**0.45)
-tE_range = np.linspace(5.5, 55, n)
+tE_range = np.linspace(1, 25, n)
 density_tE = []
 P_m2_tE =[]
 
 with open("results/robustness-run.txt", "w") as file:
 
-    for n_epochs in [72, 360]:
-        for sn_base in [23, 126.5]:
+    for n_epochs in [72, 360, 720]:
+        for sn_base in [23, 126.5, 230]:
 
 
             density_tE = []

sampling.py
@@ -249,6 +249,8 @@ def adapt_MH(model, warm_up, iterations, user_feedback = False):
     if warm_up < 5:
         raise ValueError("Not enough iterations to safely establish an empirical covariance matrix.")
     
+
+
     theta = model.center
     best_theta = deepcopy(theta)
 
@@ -559,8 +561,8 @@ def output_file(models, joint_model_chain, total_acc, n_epochs, sn, letters, nam
         file.write("\n\nResults:\n")
         for model in models:
             # models
-            P_model = model.sampled.n/joint_model_chain.n
-            sd_model = ((model.sampled.n*(1-P_model)**2 + (joint_model_chain.n-model.sampled.n)*(0-P_model)**2) / (joint_model_chain.n-1))**0.5
+            P_model = (model.sampled.n-1000)/joint_model_chain.n
+            sd_model = np.std(np.array(joint_model_chain.model_indices))#(((model.sampled.n-1000)*(1-P_model)**2 + (joint_model_chain.n-model.sampled.n+1000)*(0-P_model)**2) / (joint_model_chain.n-1))**0.5
             file.write("\n"+str(model.m)+"\nP(m|y): "+str(P_model)+r"\pm"+str(sd_model)+"\n")
 
             # parameters